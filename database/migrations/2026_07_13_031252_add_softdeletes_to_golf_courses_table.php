f7f46c822ee568ec8c8a95f6245dce2d2b5b278b
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-13 12:28:19 +0900

    コメント追加

--- database/migrations/2026_07_13_031252_add_softdeletes_to_golf_courses_table.php
+++ database/migrations/2026_07_13_031252_add_softdeletes_to_golf_courses_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('golf_courses', function (Blueprint $table) {
-            $table->softDeletes();
+            $table->softDeletes(); // deletedat カラムを追加
         });
     }
 
